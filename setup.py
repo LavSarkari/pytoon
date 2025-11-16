b7f55d404b3cbf496cdf6f44facf49c79998f581
Lav Sarkari <luv.sarkari@gmail.com>
2025-11-16 10:17:41 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name="pytoon",
+    name="tokon",
     version="0.1.0",
     author="TOON Format Contributors",
     description="Official Python implementation of the TOON serialization format",
